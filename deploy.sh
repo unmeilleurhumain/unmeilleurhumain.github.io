90b1e6b0721ece9999460f2768fd90003766d83b
Un Meilleur Humain <un_meilleur_humain@protonmail.com>
2022-05-28 10:39:17 +0700

    Test vuepress deploy

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:unmeilleurhumain/unmeilleurhumain.github.io.git main
-
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:unmeilleurhumain/unmeilleurhumain.github.io.git.git main:gh-pages
 
 cd -
